016578f449bfeee65bc393cb3d3b0325885602ef
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-04-01 20:09:53 +0700

    Add files via upload

---
+++ TP5 (Prosedur dan Fungsi)/CMIYC24/CIMYC24.c
@@ -0,0 +1,97 @@
+#include "CIMYC24.h"
+
+    
+/*membuat program*/
+
+/*int fKejarKargo(int jlari, int mlari, int totjarak, int kecepatan, int n, int jarak[51], char tempat[51][101]){
+    int i, mtotKejarKargo;
+    i = n;
+    do{
+        totjarak += jarak[i];
+        i--;
+
+    }while(i < n && strcmp(tempat[i], "abandoned_stasion") != 0);
+
+    mtotKejarKargo = jlari*60 + mlari + (float) (totjarak / (kecepatan * 60));
+
+    return mtotKejarKargo;
+}
+    
+int fJadwalKargo(int jkargo, int mkargo){
+    int mtotJadwalKargo;
+    mtotJadwalKargo = jkargo*60 + mkargo;
+
+    return mtotJadwalKargo;
+}*/
+
+
+// printf("tojar = %d\n", totjarak);
+// printf("sampe = %d\n", mtotsampekargo);
+// printf("jadkargo = %d\n", mtotjadwalkargo);
+// /*printf("%d\n", sama);
+// printf("%d\n", tidaksama);*/
+    
+/*menghasilkan output sesuai input yang telah diproses*/
+void sudoku(char st[10][10], int n, char tempat[n][101], int jarak[n], int kecepatan, int jlari, int mlari, int jkargo, int mkargo){
+    int i, j, k;
+    int mtotKejarKargo = 0;
+    int mtotJadwalKargo = 0;
+    int totjarak = 0;
+    int sama= 0, tidaksama = 0;
+    for(i = 0; i < 9; i++){
+        for(j = 0; j < 9; j++){
+            for(k = 0; k < 9; k++){
+                if(st[i][j] == st[k][j]){
+                    sama++;
+                }else{
+                    tidaksama++;
+                }
+            }
+        }
+    }
+printf("sama = %d\n", sama);
+printf("tdksama = %d\n", tidaksama);
+        i = n;
+        do{
+            totjarak += jarak[i];
+            i--;
+
+        }while(i >= 0 && strcmp(tempat[i], "abandoned_stasion") != 0);
+        mtotKejarKargo = jlari*60 + mlari + (float) (totjarak / (kecepatan * 60));
+        mtotJadwalKargo = jkargo*60 + mkargo;
+        
+printf("totjar = %d\n", totjarak);
+printf("mker = %d\n", mtotKejarKargo);
+printf("mjad = %d\n", mtotJadwalKargo);
+    if(sama == 81 && tidaksama == 648){
+        printf("Congrats, you made it out!\n");
+        //memprint pembatas
+        printf("\nooOooOooOooOooOooOooOooOooOoo\n");
+
+        
+        if(mtotKejarKargo < mtotJadwalKargo){
+            printf("\nGotcha. You cannot run from me.\n");
+        }else{
+            printf("\nThe cargo is gone. Time to find another way.\n");
+        }    
+    
+    }else{
+        printf("Almost there, afterall, mysteries are there to be solved!\n");
+    }
+}
+    /*memprint hasil game sudoku
+    if(sama == 81 && tidaksama == 648){
+        printf("Congrats, you made it out!\n");
+        //memprint pembatas
+        printf("\nooOooOooOooOooOooOooOooOooOoo\n");
+        if(mtotsampekargo < mtotjadwalkargo){
+            printf("\nGotcha. You cannot run from me.\n");
+        }else{
+            printf("\nThe cargo is gone. Time to find another way.\n");
+    }
+    }else{
+        printf("Almost there, afterall, mysteries are there to be solved!\n");
+    }
+
+    return 0;
+}*/
