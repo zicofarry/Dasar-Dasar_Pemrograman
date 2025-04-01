2bfb7fac406d5a05f9ed73a2d65fc679e604f552
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-04-01 20:18:09 +0700

    Add files via upload

---
+++ TP9 (Mesin Kata dan Karakter)/SKSN24/sksn24.h
@@ -0,0 +1,30 @@
+/*Saya Muhammad 'Azmi Salam dengan NIM 2406010
+mengerjakan tugas praktikum 9 dalam mata kuliah
+dasar pemrograman untuk keberkahan-Nya, maka saya
+tidak akan melakukan kecurangan seperti yang telah
+di spesifikasikan Aamiin.*/
+
+#include <stdio.h>
+#include <string.h>
+
+int indeks;
+int panjangkata;
+char cword[50];
+char cc;
+
+int eopc();
+void startc(char pita[]);
+void incc(char pita[]);
+void decc(char pita[]);
+void advc(char pita[]);
+int countc(char pita[]);
+int getcc();
+int eopw(char pita[]);
+void startw(char pita[]);
+void resetw();
+void incw(char pita[]);
+char* getcw();
+int getlen();
+int countw(char pita[]);
+void pisah_kata(char pita1[]);
+void kena_sensor(char pita2[]);
