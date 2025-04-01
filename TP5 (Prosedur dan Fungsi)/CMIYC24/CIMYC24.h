c25f59f996802f7940a4daf28620a7aadbb1ec42
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-04-01 20:10:12 +0700

    Add files via upload

---
+++ TP5 (Prosedur dan Fungsi)/CMIYC24/CIMYC24.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+
+/*int fKejarKargo(int jlari, int mlari, int totjarak, int kecepatan, int n, int jarak[], char tempat[51][101]);
+int fJadwalKargo(int jkargo, int mkargo);*/
+void sudoku(char st[10][10], int n, char tempat[n][101], int jarak[n], int kecepatan, int jlari, int mlari, int jkargo, int mkargo);
