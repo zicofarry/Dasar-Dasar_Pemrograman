1b9baa35c44eace8a2eacd3997dba1da88475335
Muhammad 'Azmi Salam <mhmmdzmslm36@gmail.com>
2025-04-14 12:39:27 +0700

    Add files via upload

---
+++ Kuis 3/Kuis3.soal2/mkpv24.h
@@ -0,0 +1,25 @@
+/*Saya Muhammad 'Azmi Salam dengan NIM 2406010
+mengerjakan Kuis 3 soal 2 dalam mata kuliah
+dasar pemrograman untuk keberkahan-Nya, maka saya
+tidak akan melakukan kecurangan seperti yang telah
+di spesifikasikan Aamiin.*/
+
+#include <stdio.h> // deklarasi library yang akan digunakan
+#include <string.h> // deklarasi library yang akan digunakan
+
+// deklarasi variabel global
+int idx; // index dari karakter terakhir pada kata
+int wlen; // panjang kata (word length)
+char cw[51]; // kata saat ini (current word)
+
+// bagian untuk mengakhiri, mereset, dan memulai mesin kata
+int eop(char pita[]);
+void start(char pita[]);
+void reset();
+
+// bagian memajukan dan  mesin kata
+void inc(char pita[]);
+
+// bagian mendapatkan variabel global/atribut mesin kata
+char* getcw();
+int getlen();
